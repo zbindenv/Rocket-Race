0c604700718997abb47731188b00256b36ff4a02
Weilenmann Joel <weilenmannj@ict-campus.net>
2018-04-27 11:21:26 +0200

    Rakete 1 Test

--- src/net/ictcampus/model/Rakete.java
+++ src/net/ictcampus/model/Rakete.java
@@ -3,5 +3,27 @@ package net.ictcampus.model;
 public class Rakete {
 	
 	private int speed;
+	
+	public Rakete(int speed) {
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
 
 }
